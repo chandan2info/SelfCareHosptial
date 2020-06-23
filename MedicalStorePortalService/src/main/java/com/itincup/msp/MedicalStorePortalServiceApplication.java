e40b3a213fc61b537e44c7d6653abebbf6f30611
chandan kumar <chandan2info@gmail.com>
2020-06-23 09:16:50 +0530

    first commit

---
+++ MedicalStorePortalService/src/main/java/com/itincup/msp/MedicalStorePortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.itincup.msp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalStorePortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalStorePortalServiceApplication.class, args);
+	}
+
+}
---
+++ MedicalStorePortalService/src/test/java/com/itincup/msp/MedicalStorePortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itincup.msp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalStorePortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
