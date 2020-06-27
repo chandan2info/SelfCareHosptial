a5e3da3e008d3206c30c456872494ebddacc9119
Santosh Kumar <santosh_kum@HCL.COM>
2020-06-27 19:44:33 +0530

    adding script

---
+++ MedicalStorePortalService/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO Medicine_inventory (medicine_name, quantity, price, sell_med_qantity, avail_med_quantity) VALUES
+  ('Sipla', 5000, 2000,200,4800),
+  ('SBL', 100, 5000, 10, 90),
+  ('Vicks',20, 1000, 5, 15);
