3753d27383d48cc95d1da68d5eab948224fa9bb2
chandan kumar <chandan2info@gmail.com>
2020-07-06 02:35:11 +0530

    cloud foundary settings

--- MedicalStorePortalService/src/main/java/com/itincup/msp/controller/MedicalStoreController.java
+++ MedicalStorePortalService/src/main/java/com/itincup/msp/controller/MedicalStoreController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,6 +59,7 @@ public class MedicalStoreController {
 		return iMedicine.updateMedicine(medicineName, newMedicine);
 	}
 
+	@CrossOrigin(origins = "http://localhost:4200")
 	@Transactional
 	@DeleteMapping("/medicine/{medicineName}")
 	public ResponseEntity<HttpStatus> deleteMedicine(@PathVariable("medicineName") String medicineName) {
