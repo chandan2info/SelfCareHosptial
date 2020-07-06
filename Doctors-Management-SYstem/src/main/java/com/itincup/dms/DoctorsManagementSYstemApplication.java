5d213c967dd8dd7467a31c4e04ba84dc862e8e8f
chandan kumar <chandan2info@gmail.com>
2020-07-06 09:46:08 +0530

    doctors service

---
+++ Doctors-Management-SYstem/src/main/java/com/itincup/dms/DoctorsManagementSYstemApplication.java
@@ -0,0 +1,13 @@
+package com.itincup.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsManagementSYstemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsManagementSYstemApplication.class, args);
+	}
+
+}
